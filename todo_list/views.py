42b75c6db44986326d89672363ed334fc4762a8e
Yanama20 <bokonbaevaman@gmail.com>
2025-04-12 17:49:01 +0600

    ToDo List

---
+++ todo_list/views.py
@@ -0,0 +1,16 @@
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+
+class TaskListView(ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = 'id'
+
+
+
